a0a6e3f41f980fca69895034394a6a175ea2adba
zonaetmunna <zonaetmonna@gmail.com>
2022-11-25 12:58:16 +0600

    first

--- basic/largest_number.js
+++ basic/largest_number.js
@@ -1,1 +1,30 @@
-const 
+
+// largest number compare among 3 number
+const number1 = 23;
+const number2 = 12;
+const number3 = 30;
+
+if (number1 > number2 && number1 > number3) {
+  console.log("largest number is", number1);
+}
+else if (number2 > number1 && number2 > number3) {
+  console.log("largest number is", number2);
+}
+else {
+  console.log("largest number is", number3);
+}
+
+// largest number in array
+const numbers = [2, 5, 7, 8, 4, 9, 12, 56, 43];
+const largest = numbers[0];
+
+for (var i = 0; i < arr.length; i++) {
+    if (largest < arr[i] ) {
+        largest = arr[i];
+    }
+}
+console.log(largest);
+
+for (const number of numbers) {
+  
+}
